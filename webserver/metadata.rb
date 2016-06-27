ca198ad5f94ed34feb6a84eafdbfa83e346508f8
Alan Voiski <avoiski@avenuecode.com>
2016-06-27 15:58:47 -0300

    Initial code for the webserver cookbooks

---
+++ webserver/metadata.rb
@@ -0,0 +1,7 @@
+name 'webserver'
+maintainer 'Alan Voiski'
+maintainer_email 'avoiski@avenuecode.com'
+license 'all_rights'
+description 'Installs/Configures webserver'
+long_description 'Installs/Configures webserver'
+version '0.1.0'
